b781f274706206e84da30203c82f60c4425beb9f
Cesar23-eng <a2023114080@estudiantes.upsa.edu.bo>
2025-11-16 17:02:23 -0400

    senka chupa pene 2.1

---
+++ ReservasDiscoteca.API/Migrations/20251116201305_V6_SistemaCompletoConCombosYUpdates.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservasDiscotema.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class V6_SistemaCompletoConCombosYUpdates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Boliches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImagenUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boliches", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Rol = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Combos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ImagenUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BolicheId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Combos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Combos_Boliches_BolicheId",
+                        column: x => x.BolicheId,
+                        principalTable: "Boliches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ManillaTipos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    BolicheId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManillaTipos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ManillaTipos_Boliches_BolicheId",
+                        column: x => x.BolicheId,
+                        principalTable: "Boliches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Compras",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaCompra = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TotalPagado = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TipoCompra = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EstaActiva = table.Column<bool>(type: "bit", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false),
+                    BolicheId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Compras", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Compras_Boliches_BolicheId",
+                        column: x => x.BolicheId,
+                        principalTable: "Boliches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Compras_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompraCombos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    PrecioEnElMomento = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CompraId = table.Column<int>(type: "int", nullable: false),
+                    ComboId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompraCombos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CompraCombos_Combos_ComboId",
+                        column: x => x.ComboId,
+                        principalTable: "Combos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CompraCombos_Compras_CompraId",
+                        column: x => x.CompraId,
+                        principalTable: "Compras",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompraManillas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    PrecioEnElMomento = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CompraId = table.Column<int>(type: "int", nullable: false),
+                    ManillaTipoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompraManillas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CompraManillas_Compras_CompraId",
+                        column: x => x.CompraId,
+                        principalTable: "Compras",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CompraManillas_ManillaTipos_ManillaTipoId",
+                        column: x => x.ManillaTipoId,
+                        principalTable: "ManillaTipos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mesas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreONumero = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ubicacion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PrecioReserva = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    EstaDisponible = table.Column<bool>(type: "bit", nullable: false),
+                    BolicheId = table.Column<int>(type: "int", nullable: false),
+                    CompraId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mesas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mesas_Boliches_BolicheId",
+                        column: x => x.BolicheId,
+                        principalTable: "Boliches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Mesas_Compras_CompraId",
+                        column: x => x.CompraId,
+                        principalTable: "Compras",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Combos_BolicheId",
+                table: "Combos",
+                column: "BolicheId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompraCombos_ComboId",
+                table: "CompraCombos",
+                column: "ComboId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompraCombos_CompraId",
+                table: "CompraCombos",
+                column: "CompraId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompraManillas_CompraId",
+                table: "CompraManillas",
+                column: "CompraId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompraManillas_ManillaTipoId",
+                table: "CompraManillas",
+                column: "ManillaTipoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compras_BolicheId",
+                table: "Compras",
+                column: "BolicheId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compras_UsuarioId",
+                table: "Compras",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ManillaTipos_BolicheId",
+                table: "ManillaTipos",
+                column: "BolicheId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mesas_BolicheId",
+                table: "Mesas",
+                column: "BolicheId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mesas_CompraId",
+                table: "Mesas",
+                column: "CompraId",
+                unique: true,
+                filter: "[CompraId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompraCombos");
+
+            migrationBuilder.DropTable(
+                name: "CompraManillas");
+
+            migrationBuilder.DropTable(
+                name: "Mesas");
+
+            migrationBuilder.DropTable(
+                name: "Combos");
+
+            migrationBuilder.DropTable(
+                name: "ManillaTipos");
+
+            migrationBuilder.DropTable(
+                name: "Compras");
+
+            migrationBuilder.DropTable(
+                name: "Boliches");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
